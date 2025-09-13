79af5e4475d79756ffc28884dbfe1ad541060fd6
Esraa <E.Khaled2387@nu.edu.eg>
2025-09-13 21:05:29 +0300

    LLM stable, full JSON outputs

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from llama_cpp import Llama
+import json
 
 app = FastAPI()
 
 model_path = r"C:\Users\esraa\Desktop\AI training\MIA-Final-Project-SHATO\models\mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 
-llm = Llama(model_path=model_path)
+llm = Llama(model_path=model_path, n_ctx=4096)
 
 class instruction(BaseModel):
     instruction : str
@@ -14,24 +15,29 @@ class instruction(BaseModel):
 #end point
 @app.post("/llm")
 def generate_text(input : instruction):
-    prompt = f"""
+  prompt = f"""
 You are a command generator. 
 Convert the given instruction into a JSON object following the schema below.
 
 Do not output arrays, lists, or multiple objects.
 Do not include explanations, extra text, or labels like "Instruction:" or "Output:".
 Respond with ONLY one valid JSON object.
-Always produce a single valid JSON object with opening and closing.
-Do not stop until the JSON is complete.
+Always return a single valid JSON object with opening {{ and closing }}.
 
 Important: 
 The "message" field must be a **clear, human-friendly sentence** suitable for text-to-speech. 
 It should fully describe what the robot is doing.
+The JSON object must always include the "message" field as the last key.
+Do not stop until the message is fully written and the closing brace '}}' is added.
 
 Schema:
 - Move: {{ "command": "move_to", "x": float, "y": float, "message": str }}
 - Rotate: {{ "command": "rotate", "angle": float, "direction": "clockwise|counterclockwise", "message": str }}
 - Start Patrol: {{ "command": "start_patrol", "route_id": "first_floor|bedrooms|second_floor", "speed": "slow|medium|fast", "repeat_count": -1 or integer >=1, "message": str }}
+
+Note:
+- "right" = "clockwise"
+- "left" = "counterclockwise"
 
 Examples:
 
@@ -66,7 +72,8 @@ Input: {input.instruction}
 
 Output:
 """
-    output = llm(prompt, max_tokens=300)
-    raw_text = output["choices"][0]["text"]
+  output = llm(prompt, max_tokens=200)
+  raw_text = output["choices"][0]["text"]
+  parsed = json.loads(raw_text)
 
-    return {raw_text}
+  return parsed
